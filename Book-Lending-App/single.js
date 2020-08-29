2eea7ace53aeb59fadeaa7f26426a83ee27d2df6
maduiep <emmanuelmaduip@gmail.com>
2020-08-29 02:32:35 +0100

    modified book-lending-app

--- Book-Lending-App/single.js
+++ Book-Lending-App/single.js
@@ -8,68 +8,67 @@
  * Book class
  */
 class Book {
- constructor(title) {
-  this.title = title;
-  this.available = true;
- }
+  constructor(title) {
+    this.title = title;
+    this.available = true;
+  }
 
- isAvailable() {
-  return this.available;
- }
+  isAvailable() {
+    return this.available;
+  }
 
- updateAvailability(status) {
-  this.available = status;
- }
+  updateAvailability(status) {
+    this.available = status;
+  }
 }
 
 const Store = function () {
 
- const BOOK_TITLES = ['Things fall apart', 'Rich dad, poor dad', 'Macbeth'];
+  const BOOK_TITLES = ['Things fall apart', 'Rich dad, poor dad', 'Macbeth'];
 
- class Store {
-  constructor() {
-   this.books = BOOK_TITLES.map(title => new Book(title));
+  class Store {
+    constructor() {
+      this.books = BOOK_TITLES.map(title => new Book(title));
+    }
+
+    lend(title) {
+      const book = this.books.find(book => book.title === title);
+      if (!book) {
+        console.error("This book is unavailable");
+        return;
+      }
+
+      if (!book.isAvailable()) {
+        console.error("This book has been lent out");
+        return;
+      }
+
+      book.updateAvailability(false);
+      console.log("You have successfully borrowed " + title);
+    }
+
+    returnBook(title) {
+      const book = this.books.find(book => book.title === title);
+      if (book) {
+        book.updateAvailability(true);
+        console.log(title + " has been successfully returned");
+      }
+    }
+
+    getAvailableBooks() {
+      return this.books.filter(book => book.isAvailable())
+        .map(book => book.title);
+    }
+
+    getBorrowedBooks() {
+      return this.books.filter(book => !book.isAvailable())
+        .map(book => book.title);
+    }
   }
 
-  lend(title) {
-   const book = this.books.find(book => book.title === title);
-   if (!book) {
-    console.error("This book is unavailable");
-    return;
-   }
 
-   if (!book.isAvailable()) {
-    console.error("This book has been lent out");
-    return;
-   }
-
-   book.updateAvailability(false);
-   console.log("You have successfully borrowed " + title);
-  }
-
-  returnBook(title) {
-   const book = this.books.find(book => book.title === title);
-   if (book) {
-    book.updateAvailability(true);
-    console.log(title + " has been successfully returned");
-   }
-  }
-
-  getAvailableBooks() {
-   return this.books.filter(book => book.isAvailable())
-    .map(book => book.title);
-  }
-
-  getBorrowedBooks() {
-   return this.books.filter(book => !book.isAvailable())
-    .map(book => book.title);
-  }
- }
-
-
- return new Store();
+  return new Store();
 }();
-
 
 
 
