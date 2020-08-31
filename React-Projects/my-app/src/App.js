a7e2d8dc17f3fcb2dcec5fbf454ce2c723bc4213
maduiep <emmanuelmaduip@gmail.com>
2020-09-01 00:41:49 +0100

    adding form and navbar components

--- React-Projects/my-app/src/App.js
+++ React-Projects/my-app/src/App.js
@@ -5,12 +5,45 @@ import Navbar from "./components/Navbar";
 
 //importing custom styles
 import "./styles/app.css";
+import Form from "./components/Form";
+
+const randomNumber = (limit) => Math.floor(Math.random() * limit);
 
 function App() {
+  const [page, setPage] = React.useState(0);
+  const [question, setQuestion] = React.useState("");
+  const [options, setOptions] = React.useState([]);
+  const [randOpt, setRandomOption] = React.useState(null);
+
   return (
     <div>
       <Navbar />
-      Hello World
+      {page === 0 && (
+        <Form
+          whenSubmitted={(question, options) => {
+            setPage(1);
+            setQuestion(question);
+            setOptions(options);
+            setRandomOption(randomNumber(options.length));
+            console.log("Data arrived", question, options);
+          }}
+        />
+      )}
+      {page === 1 && (
+        <span>
+          <div className="text-3xl">{question}</div>
+          {options.map((e, idx) => (
+            <li key={idx} className={idx === randOpt ? "text-red-500" : ""}>
+              {idx + 1}. {e}
+            </li>
+          ))}
+          <button type="text" onClick={() => {
+            
+          }}>
+            Ask Another Random Qestion
+          </button>
+        </span>
+      )}
     </div>
   );
 }
--- React-Projects/my-app/src/components/Form.jsx
+++ React-Projects/my-app/src/components/Form.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 
-function Form() {
-  return <div></div>;
-}
+export function Form(props) {
+  const [state, setState] = React.useState(0);
+  const [question, setQuestion] = React.useState("");
+  const [options, setOptions] = React.useState(["Hello", "Morgan", "Sinais"]);
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.whenSubmitted(question, options);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
+      <label>Question</label>
+      {/* you'll need to store the input in state */}
+      <input
+        type="text"
+        className="border border-gray-200 py-5 px-5 w-full"
+        value={question}
+        onChange={(evt) => {
+          console.log("typing", evt.target.value);
+          setQuestion(evt.target.value);
+        }}
+      />
+      <div className="flex flex-col">
+        <label>Options</label>
+        {options.map((e, idx) => (
+          <input
+            key={idx}
+            value={e}
+            className="border border-gray-200 py-5 px-5 w-full mb-2"
+            type="text"
+            onChange={(evt) => {
+              const arr = [...options];
+              arr[idx] = evt.target.value;
+              setOptions(arr);
+            }}
+          />
+        ))}
+        <div className="flex justify-between">
+          <button
+            type="button"
+            className="px-6 py-4 self-start border rounded-lg text-gray-800"
+            onClick={() => setOptions([...options, "Joseph"])}
+          >
+            + Add Options
+          </button>
+
+          <button className="px-6 py-4 self-start bg-blue-500 rounded-lg text-white">
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
 
 export default Form;
