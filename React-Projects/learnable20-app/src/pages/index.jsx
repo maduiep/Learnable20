fe3ef6d2cb2a2cfa660150ce80daf34bc88a549e
maduiep <emmanuelmaduip@gmail.com>
2020-10-15 15:53:50 +0100

    modifying About section and img, adding learnable section

--- React-Projects/learnable20-app/src/pages/index.jsx
+++ React-Projects/learnable20-app/src/pages/index.jsx
@@ -28,15 +28,37 @@ const Homepage = ()  => {
 
    <main className="genesys__main--content">
     <section className="genesys__about--section">
-     <h1>
-      Genesys provides opportunities for <br /> young Nigerian techies and <br /> opportunities to grow and <br /> innovate.
-     </h1>
+     <div className="about--text-content">
+      <h1 className="about">
+       Genesys provides opportunities for <br /> young Nigerian techies and <br /> opportunities to grow and <br /> innovate.
+      </h1>
+      <p>
+       We understand that opportunities are scarce in these parts. We <br /> are either creating or exposing our people to these <br /> opportunities.
+      </p>
+     </div>
+
+     <div className="about--image-content">
+      <diV className="about--arrow-img"></diV>
+      <div className="about--main--img"></div>
+     </div>
     </section>
-    <section className="genesys__learnable--section"></section>
+    <section className="genesys__learnable--section">
+
+     <div className="learnable--img"></div>
+
+     <div className="learnable--text-content">
+      <h1 className="learnable--main-text">Learnable</h1>
+      <p className="learnable-sub-text">
+       Every year, young Nigerians are launching new careers in the technology 
+       <br /> industry. Find out how Genesys is helping them do it.
+      </p>
+      <span><a href="#" className="learn-more">learn more</a></span>
+     </div>
+    </section>
     <section className="genesys__agora--section"></section>
     <section className="genesys__startzone--section"></section>
    </main>
-   
+
    <footer></footer>
   </>
  )
