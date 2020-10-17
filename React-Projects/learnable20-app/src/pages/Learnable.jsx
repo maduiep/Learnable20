27bb38806296e469d3a3d9f81f2463ec524035f5
maduiep <emmanuelmaduip@gmail.com>
2020-10-17 23:03:57 +0100

    adding contents in developer and esig sections

--- React-Projects/learnable20-app/src/pages/Learnable.jsx
+++ React-Projects/learnable20-app/src/pages/Learnable.jsx
@@ -88,7 +88,7 @@ const Learnable = () => {
     <section className="learnable__software">
      <div className="software__text">
       <h5>For  Developers</h5>
-      <h1>Learn to Build Software the Right Way</h1>
+      <h1>Learn to Build Software <br/> the Right Way</h1>
       <p>
         We love to build stuff. Really awesome stuff. That’s why we<br/> 
         have developed a learning program that will empower you to<br/> 
@@ -102,17 +102,65 @@ const Learnable = () => {
      </div>
 
      <div className="software__contents">
-      <diV className="software__content-1"></diV>
-      <diV className="software__content-2"></diV>
-      <diV className="software__content-3"></diV>
-      <diV className="software__content-4"></diV>
-      <diV className="software__content-5"></diV>
-      <diV className="software__content-6"></diV>
+      <diV className="software__content-1">
+        <h1 className="h1-styles">Program with <br/> JavaScript</h1>
+      </diV>
+      <diV className="software__content-2">
+       <h1 className="h1-styles">Version Control <br/> with Git</h1>
+      </diV>
+      <diV className="software__content-3">
+       <h1 className="h1-styles">Web<br/> Development<br/> with React</h1>
+      </diV>
+      <diV className="software__content-4">
+       <h1 className="h1-styles">Building APIs <br/> with NodeJS</h1>
+      </diV>
+      <diV className="software__content-5">
+       <h1 className="h1-styles">Managing Data with <br/> MongoDB</h1>
+      </diV>
+      <diV className="software__content-6">
+       <h1 className="h1-styles">Deploying with <br/> Heroku</h1>
+      </diV>
      </div>
 
      </section>
 
-    <section className="learnable__design"> <h1>Design Different</h1></section>
+    <section className="learnable__design">
+      <div className="software__contents">
+        <diV className="software__content-1">
+          <h1 className="h1-styles">Program with <br/> JavaScript</h1>
+        </diV>
+        <diV className="software__content-2">
+        <h1 className="h1-styles">Version Control <br/> with Git</h1>
+        </diV>
+        <diV className="software__content-3">
+        <h1 className="h1-styles">Web<br/> Development<br/> with React</h1>
+        </diV>
+        <diV className="software__content-4">
+        <h1 className="h1-styles">Building APIs <br/> with NodeJS</h1>
+        </diV>
+        <diV className="software__content-5">
+        <h1 className="h1-styles">Managing Data with <br/> MongoDB</h1>
+        </diV>
+        <diV className="software__content-6">
+        <h1 className="h1-styles">Deploying with <br/> Heroku</h1>
+        </diV>
+      </div>
+
+      <div className="software__text">
+      <h5>For  Designers</h5>
+      <h1>Design Different</h1>
+      <p>
+        We love to make things work perfectly. That’s why our design<br/> 
+        learning path exposes you to the science of user-oriented<br/> design.
+        <br/><br/>
+        Design is at the heart of everything we do at Genesys. Right<br/> 
+        from the start of the program, you’ll get your hands dirty as you<br/> 
+        get practically involved in the world’s most modern design<br/> 
+        concepts. At Genesys, we’re building designers who put the<br/> users first.
+      </p>
+      <h4>Learn more</h4>
+     </div>
+    </section>
 
     <section className="learnable__eligible">
       <div className="eligible--text-content">
