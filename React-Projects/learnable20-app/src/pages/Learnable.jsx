cc2d7d047e3a39871ce366732718bfd8a430e4de
maduiep <emmanuelmaduip@gmail.com>
2020-10-18 08:55:47 +0100

    resolving errors that occured in some sections

--- React-Projects/learnable20-app/src/pages/Learnable.jsx
+++ React-Projects/learnable20-app/src/pages/Learnable.jsx
@@ -190,7 +190,42 @@ const Learnable = () => {
         </p>
       </div>
     </section>
-    <section className="learnable__learning"></section>
+    <section className="learnable__learning">	
+      <div className="learning--text-content">	
+        <h1 className="learning--main-text">Learning can be fun…</h1>	
+        <p className="learning-sub-text">	
+          We believe learning should be a fun experience. For this reason, we <br/> 	
+          don’t do all the learning in the classroom.	
+          <br/><br/>	
+          The Learnable experience can not be complete without the bag of <br/> 	
+          fun times which you’ll have. To us, every task is an opportunity to <br/> 	
+          have some fun. But we do well to throw in a good number of <br/> 	
+          activities to add to this. You will find yourself partaking in a lot of <br/> 	
+          healthy activities such as cooking, sports, music, hangouts,<br/> and even random dates.	
+
+          Trust us when we say we know how to have fun.	
+        </p>	
+      </div>	
+      <div className="learning--img">	
+        <div className="learninng__img--1"></div>	
+        <div className="learninng__img--2"></div>	
+        <div className="learninng__img--3"></div>	
+        <div className="learninng__img--4"></div>	
+        <div className="learninng__img--5"></div>	
+        <div className="learninng__img--6"></div>	
+        <div className="learninng__img--7"></div>	
+        <div className="learninng__img--8"></div>	
+
+        <div className="learninng__img--9"></div>	
+        <div className="learninng__img--10"></div>	
+        <div className="learninng__img--11"></div>	
+        <div className="learninng__img--12"></div>	
+        <div className="learninng__img--13"></div>	
+        <div className="learninng__img--14"></div>	
+        <div className="learninng__img--15"></div>	
+        <div className="learninng__img--16"></div>	
+      </div>	
+    </section>
 
     <section className="learnable__experts">
       <div className="experts__focus">
@@ -249,7 +284,37 @@ const Learnable = () => {
       </div>
     </section>
     
-    <section className="learnable__classrooms"></section>
+    <section className="learnable__classrooms">	    
+      <div className="classrooms--text-content">	
+        <h1 className="classrooms--main-text">Learning doesn’t always have <br/> to be in the classroom</h1>	
+        <p className="classrooms-sub-text">	
+          We have designed other learning activities that aren’t <br/> classroom-based. 	
+          You will learn how to be a better person, how to<br/> have good conversations, 	
+          how to work better with people and most<br/> importantly, how to lead other people. 	
+          Hey! We’ll even teach you<br/> how to run a business during the Learnable Business Sessions.	
+        </p>	
+      </div>	
+      <div className="classrooms--img">	
+        <div className="classroom__img--1"></div>	
+        <div className="classroom__img--2"></div>	
+        <div className="classroom__img--3"></div>	
+        <div className="classroom__img--4"></div>	
+        <div className="classroom__img--5"></div>	
+        <div className="classroom__img--6"></div>	
+        <div className="classroom__img--7"></div>	
+        <div className="classroom__img--8"></div>	
+
+        <div className="classroom__img--9"></div>	
+        <div className="classroom__img--10"></div>	
+        <div className="classroom__img--11"></div>	
+        <div className="classroom__img--12"></div>	
+        <div className="classroom__img--13"></div>	
+        <div className="classroom__img--14"></div>	
+        <div className="classroom__img--15"></div>	
+        <div className="classroom__img--16"></div>	
+      </div>	
+    </section>
+
     <section className="learnable__apply">
       <div className="apply--img"></div>
       <div className="apply--text-content">
@@ -267,7 +332,11 @@ const Learnable = () => {
       <span><a href="#" className="learn-more">Get Started</a></span>
       </div>
     </section>
-    <section className="learnable__faq"></section>
+
+    <section className="learnable__faq">
+      <h1>FAQ</h1>
+    </section>
+
     <section className="learnable__journal">
       <div className="journal--text">
         <h1>Journal</h1>
@@ -418,3 +487,5 @@ const Learnable = () => {
 }
 
 export default Learnable
+
+
